91089085b2cdaf3bcfa155f0dfaf85bf837b3a98
MatheusHBMelo <matheushbmelo@gmail.com>
2023-11-29 20:35:15 -0300

    Feat: UserController class <FindAll>

--- src/main/java/br/com/mhbm/api/controllers/UserController.java
+++ src/main/java/br/com/mhbm/api/controllers/UserController.java
@@ -1,16 +1,16 @@
 package br.com.mhbm.api.controllers;
 
+import br.com.mhbm.api.models.User;
 import br.com.mhbm.api.models.dto.UserDTO;
 import br.com.mhbm.api.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
