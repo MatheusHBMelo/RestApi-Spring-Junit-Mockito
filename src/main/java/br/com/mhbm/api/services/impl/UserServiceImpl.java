fa6873003cb73eecdd9a76572af7ff1cd195846d
MatheusHBMelo <matheushbmelo@gmail.com>
2023-11-29 20:34:03 -0300

    Feat: UserServiceImpl class <FindAll>

--- src/main/java/br/com/mhbm/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/mhbm/api/services/impl/UserServiceImpl.java
@@ -1,17 +1,24 @@
 package br.com.mhbm.api.services.impl;
 
 import br.com.mhbm.api.models.User;
+import br.com.mhbm.api.models.dto.UserDTO;
 import br.com.mhbm.api.repositories.UserRepository;
 import br.com.mhbm.api.services.UserService;
+import br.com.mhbm.api.services.exceptions.DataIntegrityViolationException;
 import br.com.mhbm.api.services.exceptions.EmptyListException;
 import br.com.mhbm.api.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private ModelMapper mapper;
+
     @Autowired
     private UserRepository repository;
 
