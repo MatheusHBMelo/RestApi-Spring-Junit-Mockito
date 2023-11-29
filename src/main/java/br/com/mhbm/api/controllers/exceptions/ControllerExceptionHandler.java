e879457d22ea4cf5fc113dc537624bff2d5d56cc
MatheusHBMelo <matheushbmelo@gmail.com>
2023-11-29 15:40:03 -0300

    Feat: EmptyListException catch

--- src/main/java/br/com/mhbm/api/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/mhbm/api/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.mhbm.api.controllers.exceptions;
 
+import br.com.mhbm.api.services.exceptions.EmptyListException;
 import br.com.mhbm.api.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,12 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request) {
+        StandardError standardError = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+    }
+
+    @ExceptionHandler(EmptyListException.class)
+    public ResponseEntity<StandardError> emptyList(EmptyListException ex, HttpServletRequest request) {
         StandardError standardError = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
     }
