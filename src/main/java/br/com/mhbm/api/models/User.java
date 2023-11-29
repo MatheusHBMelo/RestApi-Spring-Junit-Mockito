49b171f9c01f4fa9363b44ef38f7005b64b05405
MatheusHBMelo <matheushbmelo@gmail.com>
2023-11-29 09:25:37 -0300

    Feat: User class

---
+++ src/main/java/br/com/mhbm/api/models/User.java
@@ -0,0 +1,21 @@
+package br.com.mhbm.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "TB_USER")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
