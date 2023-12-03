e539e17d510912319e7aca1d48e533e3405c60eb
MatheusHBMelo <matheushbmelo@gmail.com>
2023-12-03 19:51:14 -0300

    Feat: FindAll test methods

--- src/test/java/br/com/mhbm/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/mhbm/api/services/impl/UserServiceImplTest.java
@@ -3,17 +3,16 @@ package br.com.mhbm.api.services.impl;
 import br.com.mhbm.api.models.User;
 import br.com.mhbm.api.models.dto.UserDTO;
 import br.com.mhbm.api.repositories.UserRepository;
+import br.com.mhbm.api.services.exceptions.EmptyListException;
 import br.com.mhbm.api.services.exceptions.ObjectNotFoundException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,12 +21,14 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserServiceImplTest {
 
     public static final int ID = 1;
     public static final String NAME = "Matheus";
     public static final String EMAIL = "matheus@email.com";
     public static final String PASSWORD = "12345";
+    public static final int INDEX = 0;
     @InjectMocks
     private UserServiceImpl service;
 
@@ -48,7 +49,8 @@ class UserServiceImplTest {
     }
 
     @Test
-    @DisplayName("Deve retornar um usuário válido ao buscar por ID")
+    @DisplayName("FindById Validate")
+    @Order(1)
     void deveRetornarUsuarioExistenteAoBuscarPorId() {
         when(repository.findById(anyInt())).thenReturn(userOptional);
         User resultado = service.findById(ID);
@@ -64,8 +66,9 @@ class UserServiceImplTest {
     }
 
     @Test
-    @DisplayName("Deve retornar uma exception de ObjectNotFound ao buscar por ID")
-    void deveRetornarUmaExceptionAoBuscarPorId() {
+    @DisplayName("FindById Exception")
+    @Order(2)
+    void deveRetornarUmaExceptionAoBuscarPorIdInexistente() {
         when(repository.findById(ID)).thenThrow(new ObjectNotFoundException("Objeto de id:" + ID + " não encontrado"));
 //        try {
 //            service.findById(ID);
@@ -82,18 +85,48 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    @DisplayName("FindAll Validate")
+    @Order(3)
+    void deveRetornarUmaListaDeUsuariosValidos() {
+        when(repository.findAll()).thenReturn(List.of(user));
+        List<User> users = service.findAll();
+        assertNotNull(users);
+        assertEquals(1, users.size());
+        assertEquals(User.class, users.get(INDEX).getClass());
+        assertAll("Validações do User",
+                () -> assertEquals(ID, users.get(INDEX).getId()),
+                () -> assertEquals(NAME, users.get(INDEX).getName()),
+                () -> assertEquals(EMAIL, users.get(INDEX).getEmail()),
+                () -> assertEquals(PASSWORD, users.get(INDEX).getPassword())
+        );
+        verify(repository).findAll();
     }
 
     @Test
+    @DisplayName("FindAll Exception")
+    @Order(4)
+    void deveRetornarUmaExceptionSeListaDeUsuariosEstiverVazia() {
+        when(repository.findAll()).thenThrow(new EmptyListException("User list is empty"));
+        RuntimeException ex = Assertions.assertThrows(EmptyListException.class,
+                () -> service.findAll()
+        );
+        assertEquals(EmptyListException.class, ex.getClass());
+        assertEquals("User list is empty", ex.getMessage());
+        verify(repository).findAll();
+    }
+
+    @Test
+    @Order(5)
     void create() {
     }
 
     @Test
+    @Order(6)
     void update() {
     }
 
     @Test
+    @Order(7)
     void delete() {
     }
 
